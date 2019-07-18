5faa5b96b08adf1c3fc05fe34614768631ca57c3
Laur <laur.jicmon@gmail.com>
2019-07-18 13:01:26 +0200

    Persisting images to Database.

--- src/main/java/laur/springframework/services/ImageServiceImpl.java
+++ src/main/java/laur/springframework/services/ImageServiceImpl.java
@@ -1,15 +1,43 @@
 package laur.springframework.services;
 
+import laur.springframework.domain.Recipe;
+import laur.springframework.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.transaction.Transactional;
+import java.io.IOException;
+
 @Slf4j
 @Service
 public class ImageServiceImpl implements ImageService {
-    @Override
-    public void saveImageFile(Long recipeId, MultipartFile file) {
+    public final RecipeRepository recipeRepository;
 
-        log.debug("Received a file");
+    public ImageServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
+    @Override
+    @Transactional
+    public void saveImageFile(Long recipeId, MultipartFile file) {
+        try {
+            Recipe recipe = recipeRepository.findById(recipeId).get();
+
+            Byte[] byteObjects = new Byte[file.getBytes().length];
+
+            int i = 0;
+
+            for (byte b : file.getBytes()) {
+                byteObjects[i++] = b;
+            }
+
+            recipe.setImage(byteObjects);
+
+            recipeRepository.save(recipe);
+        } catch (IOException e) {
+            log.debug("Received a file");
+            e.printStackTrace();
+        }
     }
 }
